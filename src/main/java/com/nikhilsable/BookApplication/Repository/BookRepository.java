b09974346701509cf9f6302d267e1360086ab178
nikhisable5050 <nikhilsable5050@gmail.com>
2026-02-13 13:35:37 +0530

    chore: move backend files into backend folder

--- src/main/java/com/nikhilsable/BookApplication/Repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nikhilsable.BookApplication.Repository;
-
-import com.nikhilsable.BookApplication.Entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-    public Book findBookByTitle(String title);
-
-}
